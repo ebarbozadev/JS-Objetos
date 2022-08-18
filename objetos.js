414f112c4cb7b36b8303d55ecd7876f65fd85e92
E.Barboza <e.barboza@edu.unipar.br>
2022-08-18 00:48:12 -0300

    Adicionando estudos 18-08

---
+++ objetos.js
@@ -0,0 +1,10 @@
+const usuariosCPF = ["123456789", "123456789", "123456789"];
+
+const cliente = {
+	nome: "Emanuel Marcos",
+	idade: 18,
+	cidade: "Umuarama",
+	email: "e.barboza@edu.unipar.br"
+}
+
+// const cliente = {nome: "Emanuel Marcos", idade: 18, cidade: "Umuarama", email: "e.barboza@edu.unipar.br"}
---
+++ print-infos.js
@@ -0,0 +1,16 @@
+const aluno = {
+    nome: "EMANUEL MARCOS DOS SANTOS BARBOZA",
+    idade: 19,
+    curso: "ANÁLISE E DESENVOLVIMENTO DE SISTEMAS",
+    cpf: "123456789"
+}
+
+console.log(`Aluno: ${aluno.nome} Idade: ${aluno.idade} Curso: ${aluno.curso} CPF: ${aluno.cpf}`);
+
+const funcionario = {
+    nome: "Algacir Ramos",
+    idade: 49,
+    cpf: "987654321"
+}
+
+console.log(funcionario.cpf.substring(0, 3));
